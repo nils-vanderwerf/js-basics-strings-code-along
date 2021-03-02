c11bef037df02b14fb76e13054085aff8da9f202
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-03-02 12:38:32 +1100

    Passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = "Nils";
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase() + '!';
+console.log(excitedWelcomeMessage);
+
+const shortGreeting = "Welcome, " + currentUser[0] + "!"
+console.log(shortGreeting);
+
